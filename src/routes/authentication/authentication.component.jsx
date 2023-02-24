c14b0dd75cebe212fc79ba166740f4f8ee314f7f
Max <maxwoelfel98@googlemail.com>
2023-02-24 15:49:57 +0100

    updated authentification again

--- src/routes/authentication/authentication.component.jsx
+++ src/routes/authentication/authentication.component.jsx
@@ -1,11 +1,8 @@
-import { useEffect } from "react"
 import SignUpForm from '../../components/sign-up-form/sign-up-form.component';
 import SignInForm from '../../components/sign-in-form/sign-in-form.component';
 
 import { AuthenticationContainer } from './authentication.styles';
 
-
-// Display of the wholeSignInPage
 const Authentication = () => {
   return (
     <AuthenticationContainer>
