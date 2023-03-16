9145fc281887dabb0cb6e0d3fc6b9662e070d9ea
Max <maxwoelfel98@googlemail.com>
2023-03-16 10:08:40 +0100

    added use reducer to user conzept

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState, useEffect } from 'react';
+import { createContext, useEffect, useReducer } from 'react';
 
 import {
   onAuthStateChangedListener,
@@ -10,10 +10,40 @@ export const UserContext = createContext({
   currentUser: null,
 });
 
-export const UserProvider = ({ children }) => {
-  const [currentUser, setCurrentUser] = useState(null);
-  const value = { currentUser, setCurrentUser };
+export const USER_ACTION_TYPES = {
+  SET_CURRENT_USER: 'SET_CURRENT_USER',
+} 
 
+const userReducer = (state,action)=>{
+  console.log('dispatched',action)
+  const {type,payload} = action
+  switch(type){
+    case USER_ACTION_TYPES.SET_CURRENT_USER:
+      return{
+        ...state,
+        cureentUser: payload
+      }
+    
+    default: 
+      throw new Error (`Unabled type ${type} in userReducer`)
+  }
+}
+
+const INITAL_STATE = {
+  currentUser: null
+}
+
+export const UserProvider = ({ children }) => {
+  
+  const [state,dispatch]=useReducer(userReducer, INITAL_STATE)
+
+  const {currentUser} = state
+  console.log(currentUser)
+  const setCurrentUser = (user) =>{
+    dispatch({type: USER_ACTION_TYPES.SET_CURRENT_USER, payload: user})
+  }
+  const value = { currentUser, setCurrentUser };
+  
   useEffect(() => {
     const unsubscribe = onAuthStateChangedListener((user) => {
       if (user) {
@@ -27,3 +57,11 @@ export const UserProvider = ({ children }) => {
 
   return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
 };
+
+/*
+const userReducer = (state, action) => {
+  return {
+    currentUser: null; 
+  }
+}
+*/
