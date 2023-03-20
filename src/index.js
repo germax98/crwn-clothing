b1692a9c8bfa549dfbe7a791df43ec40606eb601
Max <maxwoelfel98@googlemail.com>
2023-03-20 10:28:09 +0100

    removed categorie and user context

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { store } from './store/store';
+
 import App from './App';
-import { CategoriesProvider } from './contexts/categories.context';
 import { CartProvider } from './contexts/cart.context';
+import { store } from './store/store';
 
 import './index.scss';
 
@@ -15,14 +15,11 @@ render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-          <CategoriesProvider>
-            <CartProvider>
-              <App />
-            </CartProvider>
-          </CategoriesProvider>
+        <CartProvider>
+          <App />
+        </CartProvider>
       </BrowserRouter>
     </Provider>
-    
   </React.StrictMode>,
   rootElement
 );
