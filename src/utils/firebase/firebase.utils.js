59634b0f9fcfc2bb269c7ac1f791dcd0b4e26e3a
Max <maxwoelfel98@googlemail.com>
2023-03-20 11:42:19 +0100

    updatet categories with reducers.

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -21,12 +21,12 @@ import {
 } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDHTreiBr4R6CnPkEwHzVSsmsMPOzLhi-Y",
-  authDomain: "crwn-clothing-db-5188d.firebaseapp.com",
-  projectId: "crwn-clothing-db-5188d",
-  storageBucket: "crwn-clothing-db-5188d.appspot.com",
-  messagingSenderId: "573016299279",
-  appId: "1:573016299279:web:d6feb97ffcd015c302d07a"
+  apiKey: 'AIzaSyDDU4V-_QV3M8GyhC9SVieRTDM4dbiT0Yk',
+  authDomain: 'crwn-clothing-db-98d4d.firebaseapp.com',
+  projectId: 'crwn-clothing-db-98d4d',
+  storageBucket: 'crwn-clothing-db-98d4d.appspot.com',
+  messagingSenderId: '626766232035',
+  appId: '1:626766232035:web:506621582dab103a4d08d6',
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
@@ -67,13 +67,7 @@ export const getCategoriesAndDocuments = async () => {
   const q = query(collectionRef);
 
   const querySnapshot = await getDocs(q);
-  const categoryMap = querySnapshot.docs.reduce((acc, docSnapshot) => {
-    const { title, items } = docSnapshot.data();
-    acc[title.toLowerCase()] = items;
-    return acc;
-  }, {});
-
-  return categoryMap;
+  return querySnapshot.docs.map((doc) => doc.data());
 };
 
 export const createUserDocumentFromAuth = async (
