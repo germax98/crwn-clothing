810328c2ab9edd440a5a1702b4277ae6e675f9d1
Max <maxwoelfel98@googlemail.com>
2023-03-23 11:30:04 +0100

    process of replacing cart.context

--- src/store/categories/categories.selector.js
+++ src/store/categories/categories.selector.js
@@ -1,10 +1,22 @@
-export const selectCategoriesMap = (state) => {
-    const categoriesMap = state.categories.categories.reduce(
-      (acc, { title, items }) => {
-        acc[title.toLowerCase()] = items;
-        return acc;
-      },
-      {}
-    );
-    return categoriesMap;
-  };
+import { createSelector } from 'reselect';
+
+const selectCategoryReducer = (state) => state.categories;
+
+export const selectCategories = createSelector(
+  [selectCategoryReducer],//the function categoriesSlice only runs if selectCategoryReducer is diffrent
+  (categoriesSlice) => categoriesSlice.categories
+);
+
+export const selectCategoriesMap = createSelector(
+  [selectCategories],
+  (categories) =>
+    categories.reduce((acc, category) => {
+      const { title, items } = category;
+      acc[title.toLowerCase()] = items;
+      return acc;
+    }, {})
+);
+
+
+
+
